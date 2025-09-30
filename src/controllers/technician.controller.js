ad34774101c17cb17ec732af6c8b4328ea773b90
kaihere14 <yashpalsharma9950@gmail.com>
2025-09-30 15:07:59 +0530

    user aggragate pipeline

--- src/controllers/technician.controller.js
+++ src/controllers/technician.controller.js
@@ -6,19 +6,47 @@ import { apiResponse } from "../utils/apiResponse.js";
 const fetchComplain = async (req, res) => {
   const { user } = req;
   const id = user._id;
+
   try {
     if (!user) {
-      throw new ApiError(409, "unable to get technician id");
+      throw new ApiError(409, "Unable to get technician id");
     }
-    const complain = await Complain.find({ technicianId: id }).select(
-      "title description user technicianId activeStatus"
-    );
-    if (!complain) {
+
+    const complains = await Complain.aggregate([
+      {
+        $match: { technicianId: id }, // filter by technician
+      },
+      {
+        $lookup: {
+          from: "users", // collection name for User model
+          localField: "user",
+          foreignField: "_id",
+          as: "userData",
+        },
+      },
+      {
+        $unwind: "$userData", // convert array -> object
+      },
+      {
+        $project: {
+          title: 1,
+          description: 1,
+          technicianId: 1,
+          activeStatus: 1,
+          "userData._id": 1,
+          "userData.name": 1,
+          "userData.email": 1,
+        },
+      },
+    ]);
+
+    if (!complains || complains.length === 0) {
       throw new ApiError(404, "No complaints found");
     }
+
     return res
       .status(200)
-      .json(new apiResponse(200, complain, "Complain fetched successfully"));
+      .json(new apiResponse(200, complains, "Complaints fetched successfully"));
   } catch (error) {
     return res
       .status(error.statusCode || 500)
